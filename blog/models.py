88fb5c68116bb882e154efd750dc96aaece5e131
SongMinSeok <songms0909@khu.ac.kr>
2023-05-30 22:12:17 +0900

    Clear HW12

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,14 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to="intruder_image/%Y/%m/%d/", default="intruder_image/default_error.png"
+    )
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -4,17 +4,26 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
 
+class IntruderImage(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "blog/post_list.html", {"posts": posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, "blog/post_detail.html", {"post": post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -25,10 +34,11 @@ def post_new(request):
             post.created_date = timezone.now()
             post.published_date = timezone.now()
             post.save()
-            return redirect('/', pk=post.pk)
+            return redirect("/", pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, "blog/post_edit.html", {"form": form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -39,7 +49,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, "blog/post_edit.html", {"form": form})
